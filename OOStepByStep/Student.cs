77b6b233f1a380e387cf82758896c5e76bc5f721
xyan8 <xyan8@slb.com>
2022-10-27 18:39:47 +0800

    feat: finish ac3-4 add student

--- OOStepByStep/Student.cs
+++ OOStepByStep/Student.cs
@@ -8,13 +8,19 @@ namespace OOStepByStep
 {
     public class Student : Person
     {
+        private string className;
         public Student(string name, int age) : base(name, age)
         {
         }
 
+        public void JionClass(string className)
+        {
+            this.className = className;
+        }
+
         public override string Introduction()
         {
-            return base.Introduction() + " I am a student.";
+            return base.Introduction() + $" I am a student of {this.className}.";
         }
     }
 }
--- OOStepByStepTest/ConsoleTest.cs
+++ OOStepByStepTest/ConsoleTest.cs
@@ -47,5 +47,20 @@ namespace OOStepByStepTest
             // then
             Assert.Equal("My name is Amy. I am 30 years old. I am a teacher.", introductionString);
         }
+
+        [Fact]
+        public void Should_return_introduction_when_student_given_class()
+        {
+            // given
+            var student = new Student("Tom", 18);
+            var className = new Class("class 2");
+
+            // when
+            className.AddStudent(student);
+            var introductionString = student.Introduction();
+
+            // then
+            Assert.Equal("My name is Tom. I am 18 years old. I am a student of class 2.", introductionString);
+        }
     }
 }
