fc657cf1d4d9f7626301854804a13c48c7900bfa
wenruiLi <36104364+wenruiLiA15@users.noreply.github.com>
2022-10-27 17:53:39 +0800

    feat: add person class and introduceself method

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
 namespace OOStepByStepTest
 {
     using OOStepByStep;
@@ -6,10 +9,37 @@ namespace OOStepByStepTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_return_msg_when_introduce_given_info()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            //given
+            string name = "Tom";
+            int age = 21;
+            Person person = new Person(name,age);
+            // when
+            string ans = "My name is Tom. I am 21 years old.";
+            string msg = person.IntroduceSelf();
+
+            // then
+
+            Assert.Equal(ans, msg);
+        }
+    }
+
+    public class Person
+    {
+        public Person(string Name, int Age)
+        {
+            this.name = Name;
+            this.age = Age;
+        }
+
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public string IntroduceSelf()
+        {
+            string msg = $"My name is {this.name}. I am {this.age.ToString()} years old.";
+            return msg;
         }
     }
 }
