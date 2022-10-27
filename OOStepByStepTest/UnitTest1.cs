35d3eebf147dcb930c15aa8e242e6a22efb2b776
wenruiLi <36104364+wenruiLiA15@users.noreply.github.com>
2022-10-27 18:15:49 +0800

    feat: add student introduce

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using System.Security.Principal;
 using Microsoft.VisualBasic;
 
 namespace OOStepByStepTest
@@ -14,10 +15,26 @@ namespace OOStepByStepTest
             //given
             string name = "Tom";
             int age = 21;
-            Person person = new Person(name,age);
+            Person person = new Person(name, age);
             // when
             string ans = "My name is Tom. I am 21 years old.";
             string msg = person.IntroduceSelf();
+
+            // then
+            Assert.Equal(ans, msg);
+        }
+
+        [Fact]
+        public void Should_return_msg_when_introduce_given_info_plus_title()
+        {
+            //given
+            string name = "Tom";
+            int age = 18;
+            string titleName = "student";
+            Student student = new Student(name, age, titleName);
+            // when
+            string ans = "My name is Tom. I am 18 years old. I am a student.";
+            string msg = student.IntroduceSelf();
 
             // then
 
@@ -25,21 +42,17 @@ namespace OOStepByStepTest
         }
     }
 
-    public class Person
+    public class Student : Person
     {
-        public Person(string Name, int Age)
+        public Student(string name, int age, string titleName) : base(name, age)
         {
-            this.name = Name;
-            this.age = Age;
         }
-
-        public string name { get; set; }
-        public int age { get; set; }
 
         public string IntroduceSelf()
         {
-            string msg = $"My name is {this.name}. I am {this.age.ToString()} years old.";
+            string msg = $"My name is {this.name}. I am {this.age.ToString()} years old. I am a student.";
             return msg;
         }
+
     }
 }
