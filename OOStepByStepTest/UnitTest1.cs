655413bc50cc69c93e9acd2a63a4531ef0298d45
hsun15 <hsun15@slb.com>
2022-10-27 17:58:02 +0800

    person introduce oneself

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -11,5 +11,16 @@ namespace OOStepByStepTest
             var class1 = new Class1();
             Assert.NotNull(class1);
         }
+
+        [Fact]
+        public void Should_return_personal_introduction_when_given_personal_info()
+        {
+            //given
+            Person person = new Person("Tom", 21);
+            //when
+            var result = person.IntroduceOneself();
+            //then
+            Assert.Equal("My name is Tom. I am 21 years old.", result);
+        }
     }
 }
